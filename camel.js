111004470391ca5d0bb5e3e300477e1bb5df322d
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-03-25 10:43:08 -0500

    Update camel.js

--- camel.js
+++ camel.js
@@ -1,4 +1,5 @@
-function camel(s){
+// Uncamel
+function uncamel(s){
  var ret;
  for(var i = 0; i < s.length; i++){
    let Maj = s[i].toUpperCase();   // UnCamel based of the Upper Case
@@ -10,4 +11,23 @@ function camel(s){
  }
  return ret;  
 }
-camel("PatrickMotsebo");
+uncamel("PatrickMotsebo");
+
+// Camel , group, grouper
+function camel(s){
+ var rep;
+for (var i = 0; i < s.length; i++){
+  if( i === 0)
+     rep = rep +s[i].toUpperCase();
+  else {
+    if(s[i] === " "){
+     rep = rep + s[i+1].toUpperCase();
+     i++
+    }
+   else
+    rep = rep + s[i];
+  }
+}
+return rep;
+}
+camel("patrick motsebo");
